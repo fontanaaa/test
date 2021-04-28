67293fffc42115b1623ae340dc24ee30f3a60422
Denis <example@gmail.com>
2021-04-28 08:57:36 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//const category = 'toys';
+
+//console.log(`http://someurl.com/${category}/5`);
+
+
+//const user = "Denis";
+
+//alert(`Privet, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
+
+    console.log(5%2);
+
+    console.log(2 + 2 * 2 === 8);
+
+    const isChecked = false,
+          isClose = false;
+          
+    console.log(isChecked || !isClose);
